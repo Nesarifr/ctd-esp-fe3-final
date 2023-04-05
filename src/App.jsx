f4bb56d49e49ed32395747fe1a652a34bb748212
Nesarifr <nestorarielfrias@gmail.com>
2023-04-04 21:44:31 -0300

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 
+import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Favs from "./Routes/Favs";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
 
 
 function App() {
   return (
       <div className="App">
-          <Navbar/>
+          <Navbar/> 
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/dentist/" element={<Detail/>}/>
+              <Route path="/contact/*" element={<Contact/>}/>
+              <Route path="/favs/*" element={<Favs/>}/>
+              <Route path="*" element={<h1>404 Not Found</h1>}/>
+          </Routes>
+
+          
           <Footer/>
       </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import styles from './Navbar.module.css'
+import { Link } from 'react-router-dom'
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
@@ -7,8 +8,23 @@ const Navbar = () => {
   return (
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      <div className={styles.navbar}>
+        <div className={styles.links}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/favs">Favs</Link>
+            </li>
+          </ul>
+        </div>
+        {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+        <button>Change theme</button>
+      </div>
     </nav>
   )
 }
